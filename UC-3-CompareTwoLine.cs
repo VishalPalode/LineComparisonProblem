00d51752327033b8c45a2d85d7bb6c51bac88e3d
VishalPalode <96374249+VishalPalode@users.noreply.github.com>
2022-01-23 14:53:14 +0530

    compare two line

---
+++ UC-3-CompareTwoLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    internal class UC_3_CompareTwoLine
+    {
+        public void compareTwoLine(String a, String b)
+        {
+            double c;
+            c = a.CompareTo(b);
+
+            if (c == 0)
+            {
+                Console.WriteLine("The lengths of two line is equal");
+            }
+            else if (c > 0)
+            {
+                Console.WriteLine("Length of first line is greater than length of second line");
+            }
+            else
+            {
+                Console.WriteLine("Length of first line is less than length of second line");
+            }
+        }
+    }
+}
